9912a842f4603d0ee426d4612b15ac968eb4a0ee
Maksim Kostromin <daggerok@gmail.com>
2017-03-29 06:41:31 +0300

    some updates and fixes

--- web/src/app/home/home.component.ts
+++ web/src/app/home/home.component.ts
@@ -6,11 +6,11 @@ import {
 } from '@angular/core';
 //
 import { HighlightJsService } from 'angular2-highlight-js';
-
-const groovyFileContent: string = require('../../../../spring-boot/groovy/src/main/groovy/daggerok/GroovyWebApplication.groovy');
-const javaFileContent: string = require('../../../../spring-boot/java/src/main/java/daggerok/JavaWebApplication.java');
-const kotlinFileContent: string = require('../../../../spring-boot/kotlin/src/main/kotlin/daggerok/KotlinWebApplication.kt');
-const scalaFileContent: string = require('../../../../spring-boot/scala/src/main/scala/daggerok/ScalaWebApplication.scala');
+//
+const groovy = require('../../../../spring-boot/groovy/src/main/groovy/daggerok/GroovyWebApplication.groovy');
+const java = require('../../../../spring-boot/java/src/main/java/daggerok/JavaWebApplication.java');
+const kotlin = require('../../../../spring-boot/kotlin/src/main/kotlin/daggerok/KotlinWebApplication.kt');
+const scala = require('../../../../spring-boot/scala/src/main/scala/daggerok/ScalaWebApplication.scala');
 
 class JvmSourceFile {
   constructor(
@@ -24,26 +24,35 @@ class JvmSourceFile {
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit, AfterViewInit {
+
   public jvmSourceFiles: JvmSourceFile[];
+
   constructor(
     private el: ElementRef,
     private service: HighlightJsService,
   ) {}
+
   public ngOnInit() {
     this.jvmSourceFiles = [
-      { lang: 'groovy', content: groovyFileContent },
-      { lang: 'java', content: javaFileContent },
-      { lang: 'kotlin', content: kotlinFileContent },
-      { lang: 'scala', content: scalaFileContent },
+      { lang: 'groovy', content: groovy },
+      { lang: 'java', content: java },
+      { lang: 'kotlin', content: kotlin },
+      { lang: 'scala', content: scala },
     ].map((raw: any, inde: number) =>
       new JvmSourceFile(raw.lang, raw.content)
     );
   }
+
   public ngAfterViewInit(): void {
-    // this.service.highlight(this.el.nativeElement.querySelector('.typescript'));
-    this.service.highlight(this.el.nativeElement.querySelector('.groovy'));
-    this.service.highlight(this.el.nativeElement.querySelector('.java'));
-    this.service.highlight(this.el.nativeElement.querySelector('.kotlin'));
-    this.service.highlight(this.el.nativeElement.querySelector('.scala'));
+    this.highlight('.groovy');
+    this.highlight('.java');
+    this.highlight('.kotlin');
+    this.highlight('.scala');
+  }
+
+  private highlight(selector: string): void {
+    this.service.highlight(
+      this.el.nativeElement.querySelector(selector)
+    );
   }
 }
